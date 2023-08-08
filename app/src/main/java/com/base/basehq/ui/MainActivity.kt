37221e5abff40cf3999cdc41ad85755cc95668ef
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-08 07:31:56 +0100

    development

--- app/src/main/java/com/base/basehq/ui/MainActivity.kt
+++ app/src/main/java/com/base/basehq/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.base.basehq.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.base.basehq.R
+import com.base.basehq.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setupWithNavController(findNavController(R.id.homeHost))
     }
 }
